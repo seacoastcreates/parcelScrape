ebc200e19779c810904540c084d5ff5c2bf3fd1d
Kirby Robinson <krobinson@mdconnectinc.com>
2019-10-24 18:50:27 -0400

    init

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+import express from 'express';
 import { getHTML, getFields } from './lib/scraper';
 
+const app = express();
+
 async function go() {
-	const html = await getHTML('http://gis.vgsi.com/PortsmouthNH/Parcel.aspx?pid=37841');
+	const html = await getHTML('https://stats.nba.com');
 	const location = await getFields(html);
 	console.log(location);
 }
+
+app.listen(2020, () => {
+	console.log('listening on 2020');
+})
 
 go();
